4d4f3212673b4e9d09a26f8d2b6e338057b8313e
Tamil Selvan <themastergts007@gmail.com>
2025-12-10 11:39:36 +1300

    Rename adwords_tool to google_ads and update to SDK v1.0.2

--- facebook/tests/context.py
+++ facebook/tests/context.py
@@ -5,4 +5,8 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../dependencies")))
 
+<<<<<<<< HEAD:facebook/tests/context.py
 from facebook import facebook
+========
+from google_ads import google_ads
+>>>>>>>> d9079d6 (Rename adwords_tool to google_ads and update to SDK v1.0.2):google_ads/tests/context.py
